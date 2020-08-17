2b6b55c8a0515f68d3b24ff2e3184b4e99cfb693
Luther Karper <luther.karper@live.com>
2020-08-17 15:01:28 -0400

    Add authRouter and AuthService

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const authRouter = require('./auth/auth-router');
 
 const app = express();
 
@@ -15,6 +16,8 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello, boilerplate!');
 });
+
+app.use('/api/auth', authRouter);
 
 app.use(function errorHandler(error, req, res, next) {
     let response;
