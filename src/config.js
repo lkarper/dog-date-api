07d6344c513a5736ae79e05fcfc0fbf2585d86f1
Luther Karper <luther.karper@live.com>
2020-08-17 10:46:46 -0400

    Add .env variables to config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-  }
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/dog-date',
+  JWT_SECRET: process.env.JWT_SECRET || '123NotAParticularyGreatSecret!!!',
+  JWT_EXPIRY: process.env.JWT_EXPIRY || '1h',
+}
