78b9a678e79ec58465a94ba93a5a5a73fb566495
Luther Karper <luther.karper@live.com>
2020-08-17 14:35:22 -0400

    Fix typo

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     "migrationDirectory": "migrations",
     "driver": "pg",
-    "connetionString": (process.env.NODE_ENV === 'test')
+    "connectionString": (process.env.NODE_ENV === 'test')
         ? process.env.TEST_DB_URL
         : process.env.DATABASE_URL,
     "ssl": !!process.env.SSL
