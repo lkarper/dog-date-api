cfd0268f8cc5dbd5a50e001f81543e1e45be0134
Luther Karper <luther.karper@live.com>
2020-08-18 12:23:39 -0400

    Remove unused boilerplate code

--- src/app.js
+++ src/app.js
@@ -14,10 +14,6 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello, boilerplate!');
-});
-
 app.use('/api/auth', authRouter);
 app.use('/api/dog-profiles', dogProfilesRouter);
 
