9ce402eaa72ef53bb661fdcc604526309c2bd92f
Luther Karper <luther.karper@live.com>
2020-08-18 12:23:12 -0400

    Update DogProfilesService
    Updated DogProfilesService methods to return an owner's profile
    information along with each dog.

--- src/dog-profiles/dog-profiles-service.js
+++ src/dog-profiles/dog-profiles-service.js
@@ -3,17 +3,52 @@ const xss = require('xss');
 const DogProfilesService = {
     getAllProfiles(db) {
         return db
-            .from('dog_date_dog_profiles')
-            .select('*');
+            .from('dog_date_dog_profiles as dp')
+            .select(
+                'dp.id', 
+                'dp.name', 
+                'dp.profile_img_url',
+                'dp.age_years',
+                'dp.age_months',
+                'dp.sex', 
+                'dp.breed',
+                'dp.weight',
+                'dp.energy',
+                'dp.temperment',
+                'dp.obedience',
+                'dp.dislikes_puppies',
+                'dp.dislikes_men',
+                'dp.dislikes_women',
+                'dp.dislikes_children',
+                'dp.recently_adopted',
+                'dp.prefers_people',
+                'dp.leash_aggression',
+                'dp.elderly_dog',
+                'dp.little_time_with_other_dogs',
+                'dp.much_experience_with_other_dogs',
+                'dp.aggressive',
+                'dp.owner_description',
+                db.raw(
+                    `json_strip_nulls(
+                        json_build_object(
+                            'id', usr.id,
+                            'username', usr.username,
+                            'email', usr.email,
+                            'phone', usr.phone
+                        )
+                    ) AS "owner"`
+                ),
+            )
+            .leftJoin('dog_date_users AS usr', 'dp.owner_id', 'usr.id')
     },
     getById(db, id) {
         return this.getAllProfiles(db)
-            .where({ id })
+            .where('dp.id', id )
             .first();
     },
     getByOwnerId(db, owner_id) {
         return this.getAllProfiles(db)
-            .where({ owner_id })
+            .where('usr.id', owner_id )
             .orderBy('name');
     },
     insertProfile(db, newProfile) {
@@ -37,7 +72,6 @@ const DogProfilesService = {
     serializeProfile(profile) {
         const {
             id,
-            owner_id, 
             name, 
             profile_img_url,
             age_years,
@@ -64,7 +98,6 @@ const DogProfilesService = {
 
         return {
             id,
-            owner_id, 
             name: xss(name), 
             profile_img_url,
             age_years,
@@ -87,6 +120,12 @@ const DogProfilesService = {
             much_experience_with_other_dogs,
             aggressive,
             owner_description: xss(owner_description),
+            owner: {
+                id: profile.owner.id,
+                email: xss(profile.owner.email),
+                username: xss(profile.owner.username),
+                phone: xss(profile.owner.phone),
+            }
         };
     },
 }
