445392cf25564ef4a4385975a05be3a8a5797574
Luther Karper <luther.karper@live.com>
2020-08-19 15:56:32 -0400

    Add getAllHowls to HowlsService

---
+++ src/howls/howls-service.js
@@ -0,0 +1,86 @@
+const HowlsService = {
+    getAllHowls(db) {
+        return db
+            .from('dog_date_howls AS ddh')
+            .select(
+                'ddh.id',
+                'ddh.user_id',
+                'ddh.howl_title',
+                'ddh.date',
+                'ddh.meeting_type',
+                'ddh.personal_message',
+                db.raw(
+                    `(
+                        select array_to_json(
+                            array_agg(row_to_json(h))
+                        ) from (
+                            select dog_id, json_strip_nulls(
+                                json_build_object(
+                                    'id', usr.id,
+                                    'username', usr.username,
+                                    'email', usr.email,
+                                    'phone', usr.phone
+                                )
+                            ) AS "owner", (
+                                select json_build_object(
+                                    'name', dp.name, 
+                                    'profile_img_url', dp.profile_img_url,
+                                    'age_years', dp.age_years,
+                                    'age_months', dp.age_months,
+                                    'sex', dp.sex,
+                                    'breed', dp.breed,
+                                    'weight', dp.weight,
+                                    'energy', dp.energy,
+                                    'temperment', dp.temperment,
+                                    'obedience', dp.obedience,
+                                    'dislikes_puppies', dp.dislikes_puppies,
+                                    'dislikes_men', dp.dislikes_men,
+                                    'dislikes_women', dp.dislikes_women, 
+                                    'dislikes_children', dp.dislikes_children,
+                                    'recently_adopted', dp.recently_adopted,
+                                    'prefers_people', dp.prefers_people,
+                                    'leash_aggression', dp.leash_aggression, 
+                                    'elderly_dog', dp.elderly_dog,
+                                    'little_time_with_other_dogs', dp.little_time_with_other_dogs,
+                                    'much_experience_with_other_dogs', dp.much_experience_with_other_dogs,
+                                    'aggressive', dp.aggressive,
+                                    'owner_description', dp.owner_description
+                                ) AS "profile"
+                                from dog_date_dog_profiles as dp
+                                where dp.id=dih.dog_id
+                            )
+                            from dog_date_dogs_in_howls AS dih
+                            where dih.howl_id=ddh.id
+                        ) h
+                    ) as dogs`
+                ),
+                db.raw(
+                    `json_strip_nulls(
+                        json_build_object(
+                            'address', ddh.address,
+                            'city', ddh.city,
+                            'state', ddh.state, 
+                            'zipcode', ddh.zipcode,
+                            'lat', ddh.lat,
+                            'lon', ddh.lon
+                        )
+                    ) AS "location"`
+                ),
+                db.raw(
+                    `(
+                        select array_to_json(
+                            array_agg(row_to_json(h))
+                        ) from (
+                            select day_of_week, start_time, end_time
+                            from dog_date_time_windows AS tw
+                            where tw.howl_id=ddh.id
+                        ) h
+                    ) as time_windows`
+                )
+            )
+            .leftJoin('dog_date_users AS usr', 'ddh.user_id', 'usr.id');
+    },
+
+};
+
+module.exports = HowlsService;
