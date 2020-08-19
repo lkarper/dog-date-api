7eccc83212dd9249600ba5eaccc35eaeb8bb06ed
Luther Karper <luther.karper@live.com>
2020-08-18 22:57:38 -0400

    Update env vars in config.js

--- src/config.js
+++ src/config.js
@@ -4,4 +4,7 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/dog-date',
   JWT_SECRET: process.env.JWT_SECRET || '123NotAParticularyGreatSecret!!!',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '1h',
+  CLOUD_NAME: process.env.CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 }
